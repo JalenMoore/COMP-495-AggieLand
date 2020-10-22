0b429e070eb318c4f1331fe9edb8c7823da0404e
James Ostmann <jsostmann@aggies.ncat.edu>
2020-10-22 13:22:26 -0400

    more chatroom updates

--- AggieLand-Secured/src/com/aggieland/Server.java
+++ AggieLand-Secured/src/com/aggieland/Server.java
@@ -4,15 +4,18 @@ package com.aggieland;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ServerEndpoint(value = "/home")
 public class Server {
 
-
+  private static final Logger LOG = Logger.getLogger(Server.class.getName());
 
   @OnOpen
   public void open(Session session) throws IOException, EncodeException {
-    session.getBasicRemote().sendText("(Server): Welcome to the chat room. Please state your username to begin.");
+    LOG.log(Level.INFO, "connection opened");
+    session.getBasicRemote().sendText("(Server): Welcome to the AggieLand Secure Chat. Please state your username to begin.");
     for(Session users: session.getOpenSessions()) {
       users.getBasicRemote().sendText("new user joined" + session.getId());
     }
@@ -25,6 +28,7 @@ public class Server {
 
   @OnMessage
   public void handleMessage(String message, Session session) throws IOException, EncodeException {
+
   }
 
 }
